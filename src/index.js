6cf1484a4a52de57eb9933f35ed304d9cbfee0ab
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-16 08:40:12 -0700

    refactor to use redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import Counter from './Counter';
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
 
 import './styles.scss';
 
-const Application = () => {
-  return (
-    <main className="Application">
-      <section className="Counters">
-        <Counter max={15} step={5} />
-      </section>
-    </main>
-  );
-};
+const initialState = {
+  count: 0
+}
 
-render(<Application />, document.getElementById('root'));
+// to avoid misspelling, we can put the actions into a variable
+// ex: const INCREMENT = 'INCREMENT' and just pass INCREMENT
+
+
+
+const reducer = (state, action) => {
+  if(action.type === "INCREMENT") {
+    return {
+      count: state.count + 1
+    }
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+class Counter extends Component {
+  render() {
+    return (
+      <main className="Counter">
+        <p classNAme="count">0</p>
+        <section className="controls">
+          <button>Increment</button>
+          <button>Decrement</button>
+          <button>Reset</button>
+        </section>
+      </main>
+    )
+  }
+}
+
+render(
+  <Provider store={store}>
+  <Counter />
+  </Provider>, 
+  document.getElementById('root')
+  
+)
